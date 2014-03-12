94ca3739e25e7d66553de8d4c3e50e76cd35f6f4
Antonis Karamitros <antonis.karamitros@gmail.com>
2014-03-12 22:06:14 +0000

    basic menu added, also a splash screen

--- main.js
+++ main.js
@@ -9,7 +9,47 @@
 			interpreter.setLang(url);
 		};
 
+		var currentActiveMenu
+			,setActiveMenu = function(menu){
+
+				if(currentActiveMenu !== undefined){
+					currentActiveMenu.class = '';
+				}
+				currentActiveMenu = menu;
+				menu.class = "pure-menu-selected";
+			};
+
+		$scope.mainMenu = [
+			{
+				text: 'Set language',
+				href: '#/setLanguage',
+				click: setActiveMenu,
+				class: ''
+			},
+			{
+				text: 'Sample language files',
+				href: '#/sampleLanguageFiles',
+				click: setActiveMenu,
+				class: ''
+			},
+			{
+				text: 'Add a new language',
+				href: '#/addNewLanguage',
+				click: setActiveMenu,
+				class: ''
+			},
+		]
+
 		$scope.keyword = interpreter.map;
+
+		$scope.splash = true;
+		$scope.mainMenuClass = 'closed';
+
+		$scope.tryIt = function(){
+		
+			$scope.mainMenuClass = '';
+			$scope.splash = false;
+		};
 	}]);
 
 }());
