91ab8d44baf6cb68351ed189fc16d2a5369673c9
Antonis Karamitros <ablackjim228@gmail.com>
2014-03-23 09:57:24 +0000

    added deployment script for Travis CI

---
+++ deploy-ghpages.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+( cd gh-pages
+ git init
+ git config user.name "Travis-CI"
+ git config user.email "travis@nodemeatspace.com"
+ git add .
+ git commit -m "Deployed to Github Pages"
+ git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+)
