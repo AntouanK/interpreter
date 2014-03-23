e905deec7da42a56d3575d4c558b3897bcae09ef
Travis-CI <travis@nodemeatspace.com>
2014-03-23 17:55:51 +0000

    clean duplicate lang files

--- deploy-ghpages.sh
+++ deploy-ghpages.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-( cd gh-pages
+(
+ cd gh-pages
  git init
  git config user.name "Travis-CI"
  git config user.email "travis@nodemeatspace.com"
  git add .
  git commit -m "Deployed to Github Pages"
  git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+ echo done;
 )
+exit 0;
