5fea37f4aeeaab1c4adeb8d5afc853326c16a0ff
Travis-CI <travis@nodemeatspace.com>
2014-03-23 18:16:06 +0000

    travis commit testing

--- deploy-ghpages.sh
+++ deploy-ghpages.sh
@@ -5,7 +5,7 @@
  git config user.name "Travis-CI"
  git config user.email "travis@nodemeatspace.com"
  git add .
- git commit -m "Deployed to Github Pages @ ${date}"
+ git commit -m "Deployed to Github Pages @ ${DATE}"
  git push -f -branch=gh-pages "https://${GH_TOKEN}@${GH_REF}" > /dev/null 2>&1
  echo done;
 )
